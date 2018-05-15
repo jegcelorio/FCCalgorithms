689ffca80e775d3496350016743b180e151e2bb2
Jorge Celorio <jegcelorio@yahoo.com>
2018-05-15 15:04:12 -0500

    Add files via upload

---
+++ factorialize.js
@@ -0,0 +1,32 @@
+// version using for loop
+
+function factorialize(num){
+  factorial = 1;
+
+  if(num == 0){
+    return 1;
+  } else if (num == 1){
+    return 1;
+  }
+
+  for (var i=num; i>=1; i-- ){
+    factorial = factorial * i;
+  }
+  num = factorial;
+  return num;
+}
+
+factorialize(1);
+
+// version using recursion
+function factorialize(num){
+
+  if(num==0){
+    return 1;
+  }
+
+  if(num == 1){
+    return 1;
+  }
+  return num*factorialize(num-1);
+}
